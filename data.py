d94f047265319938b90cbb7a86d4345534d2fbdd
Yasmin Prudente <yasminpnogueira@hotmail.com>
2025-10-29 17:58:42 -0300

    New Commit

--- data.py
+++ data.py
@@ -1,1 +1,10 @@
-# Arquivo com constantes
+#constante
+URBAN_ROUTES_URL = 'https://cnt-6ddb7fa1-ee25-4bfe-99d5-37dc72607c17.containerhub.tripleten-services.com?lng=pt'
+
+#dados de teste
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Pare no bar de sucos'
--- main.py
+++ main.py
@@ -1,2 +1,53 @@
+import data
+import helpers
 
 class TestUrbanRoutes:
+    @classmethod
+    def setup_class(cls):
+        # Verifica se o servidor Urban Routes está ativo
+        if helpers.is_url_reachable(data.URBAN_ROUTES_URL):
+            print("Conectado ao servidor Urban Routes")
+        else:
+            print("Não foi possível conectar ao Urban Routes. Verifique se o servidor está ligado e ainda em execução.")
+
+    def test_set_route(self):
+        # Adicionar em S8
+        print("função criada para definir a rota")
+        pass
+
+    def test_select_plan(self):
+        # Adicionar em S8
+        print("função criada para selecionar plano")
+        pass
+
+    def test_fill_phone_number(self):
+        # Adicionar em S8
+        print("função criada para preencher telefone")
+        pass
+
+    def test_fill_card(self):
+        # Adicionar em S8
+        print("função criada para preencher cartão")
+        pass
+
+    def test_comment_for_driver(self):
+        # Adicionar em S8
+        print("função criada para comentário ao motorista")
+        pass
+
+    def test_order_blanket_and_handkerchiefs(self):
+        # Adicionar em S8
+        print("função criada para testar pedido de cobertores e lençois")
+        pass
+
+    def test_order_2_ice_creams(self):
+        # Adicionar em S8
+        print("função criada para testar pedido de sorvete")
+        for i in range(2):
+            # Adicionar em S8
+            pass
+
+    def test_car_search_model_appears(self):
+        # Adicionar em S8
+        print("função criada para testar busca de carro")
+        pass
